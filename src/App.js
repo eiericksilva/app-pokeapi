6a753f8ceac136fc43d2d01ae39b218175992741
Erick Oliveira <ericksilvaredes@gmail.com>
2022-11-16 23:21:58 -0300

    Consumindo e renderizando name dos Pokemons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [pokemons, setPokemons ] = useState([])
+
+  const offset = 0;
+  const limit = 10;
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+  useEffect(() => {
+
+    fetch(url)
+      .then( res => res.json())
+      .then( jsonRes => jsonRes.results)
+      .then( pokemons => setPokemons(pokemons))
+      .catch( err => console.log(err))
+
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Pokemon List</h4>
+      <ol> 
+        {pokemons.map((pokemon) => ( <li>{pokemon.name}</li> ))}
+      </ol>
     </div>
   );
 }
